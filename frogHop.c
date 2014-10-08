7efbc7f4b4019366b5fb2fb3d190884172f7a43e
iMiiTH <mitchell.mohorovich@gmail.com>
2014-10-08 15:57:33 -0400

    Removed memory checks. (Not needed.)

--- frogHop.c
+++ frogHop.c
@@ -9,34 +9,35 @@ typedef struct PondType {
 
 Pond* initializePond(int size);
 void printPond(Pond *P);
-int insufficientMemory();
 
 int main( int argc, char *argv[] )
 {
+   if(argv[1]==NULL || argv[2]==NULL || argv[3]==NULL) {
+      printf("Invalid arguments... exiting.\n");
+      return 1;
+   }
    int size = atoi(argv[1]);
    int xPos = atoi(argv[2]);
    int yPos = atoi(argv[3]);
-
+   
    printf("The grid will be %dX%d\n",size, size);
    printf("The starting position is: %d, %d\n", xPos, yPos); 
 
    Pond *pond;
+   printf("%p\n", pond);
    pond=initializePond(size);
-   if(pond==NULL) return insufficientMemory();
    printPond(pond);
+   return 0;
 }
 
 Pond* initializePond(int size)
 {
    int i,j;
    Pond *P=malloc(sizeof(Pond));
-   if(P==NULL) return NULL;
    P->length=size;
    P->grid=calloc(size, sizeof *P->grid);
-   if(P->grid==NULL) return NULL;
    for(i=0; i<P->length; i++) {
       P->grid[i]=calloc(size, sizeof(int));
-      if(P->grid[i]==NULL) return NULL;
    }
    return P;
 }
@@ -50,9 +51,4 @@ void printPond(Pond *P)
       }
       putchar('\n');
    }
-}
-int insufficientMemory()
-{
-   printf("Not enough memory.\n");
-   return NULL;
 }
